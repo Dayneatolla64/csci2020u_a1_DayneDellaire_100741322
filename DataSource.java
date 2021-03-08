4fe085496b1ea5ae3d9d29708c1fbf57aa5f23c3
Dayneatolla64 <71149791+Dayneatolla64@users.noreply.github.com>
2021-03-08 16:02:25 -0500

    Update DataSource.java

--- DataSource.java
+++ DataSource.java
@@ -6,6 +6,11 @@ import javafx.collections.ObservableList;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+Turns the ArrayList into a FxCollections obserableArrayList 
+(I know I didn't need another function for this but I wasn't able to get it working and when I solved the
+issue I was too lazy to delete this file).
+*/
 public class DataSource {
     public static ObservableList<TestFile> getAllTests(List<TestFile> temp) {
         return FXCollections.observableArrayList(temp);
