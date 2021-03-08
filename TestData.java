46279003a3b46f632d6131db0e844ddd678f4e76
Dayneatolla64 <71149791+Dayneatolla64@users.noreply.github.com>
2021-03-08 16:14:45 -0500

    Update TestData.java

--- TestData.java
+++ TestData.java
@@ -6,15 +6,15 @@ import java.io.*;
 import java.util.*;
 
 public class TestData{
-    private Map<String, Integer> spamWordFreq;
-    private Map<String, Integer> hamWordFreq;
-    private Map<String, Double> spamProbMap;
-    private Map<String, Double> hamProbMap;
+    private Map<String, Integer> spamWordFreq; //Stores how frequent a word appears among all the files
+    private Map<String, Integer> hamWordFreq; //Stores how frequent a word appears among all the files
+    private Map<String, Double> spamProbMap; //Probability map for spam files
+    private Map<String, Double> hamProbMap; //Probability map for ham files
     private Map<String, Double> probabilities;
-    private List<TestFile> testClasses = new ArrayList<TestFile>();
-    private int amountOfSpam;
-    private int amountOfHam;
-
+    private List<TestFile> testClasses = new ArrayList<TestFile>(); //Holds all TestFile objects
+    private int amountOfSpam; //Total amount of spam files
+    private int amountOfHam; //Total amount of ham files
+    
     public TestData(Map<String, Double> spamProbMap, Map<String, Double> hamProbMap) {
         spamWordFreq = new TreeMap<>();
         hamWordFreq = new TreeMap<>();
@@ -26,7 +26,17 @@ public class TestData{
     public Map<String, Double> getProbabilities(){return probabilities;}
     public void setProbabilities(Map<String, Double> probabilities){this.probabilities = probabilities;}
     public List<TestFile> getClasses(){return testClasses;}
-
+    
+    /*
+    readFile
+    @File
+    @Map<String, Integer>
+    
+    This method will take a file and a map and fill up the testFile list with TestFiles. It does this
+    by reading each file word for word and storing the words of the current file into map. It then
+    passes the map to the calculateProbabilities method which returns a the probability for the TestFile
+    object. It then creates a TestFile object and stores it in the list. This method repeats for all documents
+    */
     public void readFile(File file, Map<String, Integer> map) throws IOException{
         //System.out.println("Starting parsing the file:" + file.getAbsolutePath());
         map = new TreeMap<>();
@@ -57,7 +67,8 @@ public class TestData{
             testClasses.add(new TestFile(file.getName(), calculateProbability(map), file.getParentFile().getName()));
         }
     }
-
+    
+    //Taken from Course Examples
     private void countWord(String word, Map<String, Integer> map){
         if(map.containsKey(word)){
             int previous = map.get(word);
@@ -66,12 +77,24 @@ public class TestData{
             map.put(word, 1);
         }
     }
-
+    
+    //Taken from Course Examples
     private boolean isValidWord(String word){
         String allLetters = "^[a-zA-Z]+$";
         return word.matches(allLetters);
     }
+    
+    /*
+    calculateProbability
 
+    @Map<String, Integer>
+    
+    This method will take a map which contains all word in a file and do
+    calculations to determine the probability that the given file is spam
+    or not (doesnt work)
+    
+    @return double
+    */
     private Double calculateProbability(Map<String, Integer> map){
         int totalWords = 0;
         double n = 0.0;
@@ -98,7 +121,14 @@ public class TestData{
 
         return 1/(1+Math.pow(Math.E,Math.abs(n)));
     }
-
+    
+    /*
+    calculateAccuracy
+    
+    This method will do calculations to determine the accuracy of the program
+    
+    @return double
+    */
     public Double calculateAccuracy(){
         int numOfFiles = amountOfHam + amountOfSpam;
         int numTruePositives = 0;
@@ -120,7 +150,14 @@ public class TestData{
 
         return ((double)numTruePositives + (double)numTrueNegatives)/numOfFiles;
     }
-
+    
+    /*
+    calculatePrecision
+    
+    This method will do calculations to determine the precision of the program
+    
+    @return double
+    */
     public Double calculatePrecision(){
         int numOfFiles = amountOfHam + amountOfSpam;
         int numTruePositives = 0;
@@ -142,7 +179,15 @@ public class TestData{
 
         return (double)numTruePositives/((double)numFalsePositives + (double)numTruePositives);
     }
-
+    
+    /*
+    readDirectory
+    
+    @String path
+    
+    This method will take a String that represents the main path and will go into both the
+    spam and ham folders so each has its words counted and probabilities generated
+    */
     public void readDirectory(String path){
         File file = new File(path);                                                         //Given a path to either Test or Trial locate spam and ham
 
